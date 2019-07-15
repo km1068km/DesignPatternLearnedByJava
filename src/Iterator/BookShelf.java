3d974bd40c9cee676d790de39aab5c044d29fc06
km1068km <moteko.0608.mrchildren@gmail.com>
2019-07-15 14:03:46 +0900

    Iterator １つ１つ数え上げる

---
+++ src/Iterator/BookShelf.java
@@ -0,0 +1,31 @@
+package Iterator;
+
+public class BookShelf implements Aggregate {
+
+
+    private Book[] books;
+
+    private int last;
+
+    public BookShelf(int maxSize){
+        this.books = new Book[maxSize];
+    }
+
+    public Book getBookAt(int index){
+        return books[index];
+    }
+
+    public  void appendBook(Book book){
+        this.books[last] = book;
+        last++;
+    }
+
+    public int getLength(){
+        return last;
+    }
+
+    public Iterator iterator(){
+        return new BookShelfIterator(this);
+    }
+
+}
